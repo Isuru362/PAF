06113275c049f3bc97c358d8c794253cf1f39064
Isuru362 <isuruprabhath6@gmail.com>
2022-05-15 23:46:59 +0530

    done

--- src/model/EbillsAPI.java
+++ src/model/EbillsAPI.java
@@ -1,5 +1,7 @@
 
-package model;import java.io.IOException;
+package model;
+
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +11,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 /**
-* Servlet implementation class EmployeesAPI
-*/@WebServlet("/EmployeesAPI")
-public class EmployeesAPI extends HttpServlet {
+* Servlet implementation class EbillsAPI
+*/@WebServlet("/EbillsAPI")
+public class EbillsAPI extends HttpServlet {
 private static final long serialVersionUID = 1L;
-Ebill employeeObj = new Ebill(); //convert request parameters to a map
+Ebill employeeObj = new Ebill(); //convert request parameters to the map
 private static Map<String, String> getParasMap(HttpServletRequest request) {
 Map<String, String> map = new HashMap<String, String>(); try {
 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
@@ -27,7 +29,7 @@ map.put(p[0], p[1]);
 /**
 * @see HttpServlet#HttpServlet()
 */
-public EmployeesAPI() {
+public EbillsAPI() {
 super();
 } /**
 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,11 +41,12 @@ response.getWriter().append("Served at: ").append(request.getContextPath());
 */
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //sending values to insert function
-String output = employeeObj.insertEmployee( request.getParameter("employeename"),
-request.getParameter("employeedob"),
-request.getParameter("employeeaddress"),
-request.getParameter("employeegender"),
-request.getParameter("employeesalary"));
+String output = ebillObj.insertEbill( request.getParameter("ebillid"),
+request.getParameter("accno"),
+request.getParameter("amount"),
+request.getParameter("startdate"),
+request.getParameter("enddate");
+request.getParameter("refno"));
 //sending the output to client
 response.getWriter().write(output);
 }
@@ -53,12 +56,13 @@ response.getWriter().write(output);
 protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //parameter map
 Map<?, ?> paras = getParasMap(request); //getting values from the map and sending to update function
-String output = employeeObj.updateEmployee(paras.get("hidEmployeeIDSave").toString(),
-paras.get("employeename").toString(),
-paras.get("employeedob").toString(),
-paras.get("employeeaddress").toString(),
-paras.get("employeegender").toString(),
-paras.get("employeesalary").toString());
+String output = employeeObj.updateEmployee(paras.get("hidEbillIDSave").toString(),
+paras.get("ebillid").toString(),
+paras.get("accno").toString(),
+paras.get("amount").toString(),
+paras.get("startdate").toString(),
+paras.get("enddate").toString(),
+paras.get("refno").toString());
 //sending the output to client
 response.getWriter().write(output);
 }
@@ -68,7 +72,7 @@ response.getWriter().write(output);
 protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	//parameter map
 	Map<?, ?> paras = getParasMap(request); //getting values from the map and sending to delete function
-	String output = employeeObj.deleteEmployee( paras.get("employeeid").toString());
+	String output = ebillObj.deleteEbill( paras.get("ebillid").toString());
 	//sending the output to client
 	response.getWriter().write(output);
 	}}
